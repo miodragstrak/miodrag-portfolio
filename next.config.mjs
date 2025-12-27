62ca09321f55aafedf1452484051cedca7981d22
Miodrag Strak <miodrag.strak@gmail.com>
2025-12-27 14:36:47 +0100

    New config change

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,9 @@
-const isProd = process.env.NODE_ENV === "production";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-
   images: {
     unoptimized: true,
   },
-
-  basePath: isProd ? "/miodrag-portfolio" : "",
-  assetPrefix: isProd ? "/miodrag-portfolio/" : "",
 };
 
 export default nextConfig;
