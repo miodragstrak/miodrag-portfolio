d43efe12368ad242a460e9d20985360ddb2be7a5
Miodrag Strak <miodrag.strak@gmail.com>
2025-12-27 14:29:13 +0100

    Config change

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const isProd = process.env.NODE_ENV === "production";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+
   images: {
     unoptimized: true,
   },
